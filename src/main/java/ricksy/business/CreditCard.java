9907c5f5d870e9ea59082477e51d01c6c5a7ca3a
Alexgb24 <agonzalez@cifpfbmoll.eu>
2020-02-20 18:34:02 +0100

    Credit card

---
+++ src/main/java/ricksy/business/CreditCard.java
@@ -0,0 +1,44 @@
+package ricksy.business;
+
+public class CreditCard {
+	
+
+	private String owner = null;
+	private String number = null;
+	private double credit = 3000d;
+	private String symbol = EZI;
+	
+
+	CreditCard(String owner, String number) {
+		this.owner = owner;
+		this.number = number;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public String number() {
+		return number;
+	}
+
+	public double credit() {
+		return credit;
+	}
+	
+	public boolean pay(double payment) {
+		if (credit - payment != credit)
+			return true;
+		else
+			return false;
+	}
+	
+    @Override
+    public String toString() {
+        StringBuilder creditcard = new StringBuilder();
+        creditcard.append("owner=" + getOwner());
+        creditcard.append(", number=" + number());
+        creditcard.append(", credit=" + credit());
+        return creditcard.toString();
+    }
+}
