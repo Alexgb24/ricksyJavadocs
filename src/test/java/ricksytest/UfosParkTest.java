0a38fb538657114e1137aa9a9c45d3ccca769b96
Alexgb24 <56043310+Alexgb24@users.noreply.github.com>
2020-03-04 10:45:45 +0100

    UfosTest

--- src/test/java/ricksytest/UfosParkTest.java
+++ src/test/java/ricksytest/UfosParkTest.java
@@ -12,48 +12,75 @@ public class UfosParkTest {
     String[] ovnis = { "unx", "dox", "trex" };
 
     /**
-     * Testea el metodo añadir ovni 
+     * Testea el metodo aÃ±adir ovni 
      * a la flota de ufos
      */
     @Test
     public void addUfoTest() {
+    	UfosPark ufos = new UfosPark();
     }
 
     /**
      * Testea el metodo reservar ovni 
      * y cargar el pago en la tarjeta 
-     * de crédito.
-     * El crédito de la tarjeta varía.
+     * de crÃ©dito.
+     * El crÃ©dito de la tarjeta varÃ­a.
      */
     @Test
     public void dispatchTest() {
+    	UfosPark ufos = new UfosPark();
+    	CreditCard crecard = new CreditCard("Abradolph", "482948193");
+    	ufos.add("dox");
+    	ufos.dispatch(crecard);
+    	assertEquals("dox", ufos.getUfoOf(crecard.number));
+    	
+    	
     }
 
     /**
      * Testea que no se pueda reservar un ovni 
-     * si no hay crédito suficiente en la tarjeta.
-     * El crédito de la tarjeta no varía.
+     * si no hay crÃ©dito suficiente en la tarjeta.
+     * El crÃ©dito de la tarjeta no varÃ­a.
      */
     @Test
     public void dispatchNoCreditTest() {
+    	UfosPark ufos = new UfosPark();
+    	CreditCard crecard = new CreditCard("Abradolph", "482948193");
+    	ufos.add("dox");
+    	crecard.pay(3000);
+    	ufos.dispatch(crecard);
+    	assertEquals(null, ufos.getUfoOf(crecard.number));
     }
 
     /**
      * Testea que no se pueda reservar un ovni 
      * si ya existe un ovni reservado para esa tarjeta.
-     * El crédito de la tarjeta no varía.
+     * El crÃ©dito de la tarjeta no varÃ­a.
      */
     @Test
     public void dispatchUfoAlreadyReservedTest() {
+    	UfosPark ufos = new UfosPark();
+    	CreditCard crecard = new CreditCard("Abradolph", "482948193");
+    	ufos.add("dox");
+    	ufos.dispatch(crecard);
+    	assertEquals("dox", ufos.getUfoOf(crecard.number));
+    	ufos.add("trex");
+    	ufos.dispatch(crecard);
+    	assertEquals("dox", ufos.getUfoOf(crecard.number));
     }
 
     /**
      * Testea que no se pueda reservar un ovni 
      * si no existe ninguno disponible (sin reservar).
-     * El crédito de la tarjeta no varía.
+     * El crÃ©dito de la tarjeta no varÃ­a.
      */
     @Test
     public void dispatchNoUfoAvaliableTest() {
+    	UfosPark ufos = new UfosPark();
+    	CreditCard crecard = new CreditCard("Abradolph", "482948193");
+    	ufos.add(null);
+    	ufos.dispatch(crecard);
+    	assertEquals(null, ufos.getUfoOf(crecard.number));
     }
 
     /**
@@ -61,5 +88,10 @@ public class UfosParkTest {
      */
     @Test
     public void getUfoOfTest() {
+    	UfosPark ufos = new UfosPark();
+    	CreditCard crecard = new CreditCard("Abradolph", "482948193");
+    	ufos.add("dox");
+    	ufos.dispatch(crecard);
+    	assertEquals("dox", ufos.getUfoOf(crecard.number));
     }
 }
